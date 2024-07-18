0492f93cf27ebe1261e74fc1e7c9fa2e8bb0dbda
dannesx <daniel.viantunes@gmail.com>
2024-07-18 16:31:15 -0300

    fix

--- ExecutionPolicy.ps1
+++
@@ -1,2 +0,0 @@
-# Definir política de execução para o processo atual
-Set-ExecutionPolicy Bypass -Scope Process -Force
--- Set-Wallpapers.ps1
+++ Set-Wallpapers.ps1
@@ -1,15 +1,12 @@
-# Definir política de execução para o processo atual
-Set-ExecutionPolicy Bypass -Scope Process -Force
+# Caminho da imagem que você deseja definir como tela de fundo
+$wallpaperPath = ".\Wallpapers\Wallpaper.jpg"
 
-# Caminho relativo da imagem que você deseja definir como tela de fundo
-$relativeWallpaperPath = ".\Wallpapers\Wallpaper.jpg"
-
-# Caminho relativo da imagem que você deseja definir como tela de bloqueio
-$relativeLockScreenPath = ".\Wallpapers\Bloqueio.jpg"
+# Caminho da imagem que você deseja definir como tela de bloqueio
+$lockScreenPath = ".\Wallpapers\Bloqueio.jpg"
 
 # Resolver caminhos relativos para caminhos absolutos
-$wallpaperPath = Resolve-Path $relativeWallpaperPath
-$lockScreenPath = Resolve-Path $relativeLockScreenPath
+$wallpaperPath = Resolve-Path $wallpaperPath
+$lockScreenPath = Resolve-Path $lockScreenPath
 
 # Definir a tela de fundo
 function Set-Wallpaper {
@@ -35,22 +32,25 @@ function Set-LockScreenImage {
         [string]$path
     )
 
+    # Verificar se a chave do registro existe, se não, criar
+    $regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+    if (-Not (Test-Path $regPath)) {
+        New-Item -Path $regPath -Force
+    }
+
     $bytes = [System.IO.File]::ReadAllBytes($path)
     $encodedImage = [System.Convert]::ToBase64String($bytes)
 
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP" -Name "LockScreenImageStatus" -Value 1 -Type Dword
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP" -Name "LockScreenImageUrl" -Value $path -Type String
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP" -Name "LockScreenImageUrlStatus" -Value 1 -Type Dword
+    Set-ItemProperty -Path "$regPath" -Name "LockScreenImageStatus" -Value 1 -Type Dword
+    Set-ItemProperty -Path "$regPath" -Name "LockScreenImageUrl" -Value $path -Type String
+    Set-ItemProperty -Path "$regPath" -Name "LockScreenImageUrlStatus" -Value 1 -Type Dword
 
     $UserKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Lock Screen"
     if (-Not (Test-Path $UserKey)) {
-        New-Item -Path $UserKey
+        New-Item -Path $UserKey -Force
     }
     Set-ItemProperty -Path $UserKey -Name "CreativeId" -Value $encodedImage -Type String
 }
 
 Set-Wallpaper -path $wallpaperPath
 Set-LockScreenImage -path $lockScreenPath
-
-# Manter a janela aberta após a execução dos comandos
-Read-Host -Prompt "Pressione Enter para fechar esta janela"
