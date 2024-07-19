23bab2d5f9d28a795c51f12599ad0ce10c98eec6
dannesx <daniel.viantunes@gmail.com>
2024-07-19 14:59:59 -0300

    add postgresql

--- src/scripts/Install-Programs.ps1
+++ src/scripts/Install-Programs.ps1
@@ -5,10 +5,13 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Instalar pacotes usando Chocolatey
-choco install adobereader git firacode vscode winrar arduino blender lmms nodejs python3 unity-hub xampp-74 googledrive googlechrome insomnia-rest-api-client teamviewer vlc -y
+choco install adobereader git firacode vscode winrar arduino blender lmms nodejs python3 unity-hub xampp-74 googledrive googlechrome insomnia-rest-api-client teamviewer vlc pgadmin4 -y
 
 # Instalar o Godot na versão 3.5.1
 choco install godot --version=3.5.1 -y
+
+# Instalar o Postgresql e definir a senha padrão
+choco install postgresql --params '/Password:postgres'
 
 # Função para instalar aplicativos usando winget
 function Install-App {
